7161f8b07f65e5ed0cce905c9f832996fa819a94
Ahmad <Ahmad.Ammar.Almohmmad@hotmail.com>
2023-02-01 21:59:06 +0200

    comm Ahmad

--- lib/widget/comment.dart
+++ lib/widget/comment.dart
@@ -2,6 +2,7 @@ import 'package:comment_box/comment/comment.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
 
 late User signedInUser; //this will give us the email
 final _firestore = FirebaseFirestore.instance;
@@ -15,13 +16,13 @@ class _commentState extends State<comment> {
   final _auth = FirebaseAuth.instance;
   final formKey = GlobalKey<FormState>();
   final TextEditingController commentController = TextEditingController();
-  final Stream<QuerySnapshot> _usersStreamPackage = FirebaseFirestore.instance
-      .collection('comment') as Stream<QuerySnapshot<Object?>>;
-  final CollectionReference<Map<String, dynamic>> userStream =
-      FirebaseFirestore.instance.collection('comment');
+  
+    CollectionReference userref =
+      FirebaseFirestore.instance.collection("comment");
   String? messageText;
   void initState() {
     super.initState();
+    fetchData();
     getCurrentUser();
   }
 
@@ -74,59 +75,49 @@ class _commentState extends State<comment> {
     },
   ];
 
-  Widget commentChild(data) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: _usersStreamPackage,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator();
-        }
-
-        return ListView(
-          children: [
-            for (var i = 0; i < snapshot.data!.docs.length; i++)
-              Padding(
-                padding: const EdgeInsets.fromLTRB(2.0, 8.0, 2.0, 0.0),
-                child: ListTile(
-                  leading: GestureDetector(
-                    onTap: () async {
-                      // Display the image in large form.
-                      print("Comment Clicked");
-                    },
-                    child: Container(
-                      height: 50.0,
-                      width: 50.0,
-                      decoration: new BoxDecoration(
-                          color: Colors.black87,
-                          borderRadius:
-                              new BorderRadius.all(Radius.circular(50))),
-                      child: CircleAvatar(
-                          radius: 50,
-                          backgroundImage: CommentBox.commentImageParser(
-                              imageURLorPath:
-                                  'https://cdn.onlinewebfonts.com/svg/img_322817.png')),
-                    ),
+  Widget commentChild() {
+    return 
+     FutureBuilder(
+          future: fetchData(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return GridView.builder(
+                 scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  gridDelegate:
+                      const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
                   ),
-                  title: Text(
-                    "${snapshot.data!.docs[i]['user_name']}",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text(
-                    "${snapshot.data!.docs[i]["text"]}",
-                  ),
-                  trailing: Text("${snapshot.data!.docs[i]["date"]}",
-                      style: TextStyle(fontSize: 10)),
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int i) {
+                    return 
+                    Container(
+                      height: 200,
+                      color: Color.fromARGB(255, 88, 83, 83),
+                      child: ListTile(
+                                    leading: const Icon(Icons.comment),
+                                    trailing:  Text(
+                                      "",
+                                      style: TextStyle(color: Colors.green, fontSize: 15),
+                                    ),
+                                    title: Text("${snapshot.data[i]['text']}")),
+                    );
+                  });
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  '${snapshot.error} occurred',
+                  style: TextStyle(fontSize: 18),
                 ),
-              )
-          ],
-        );
-      },
-//this is the end of stream builder
-    );
+              );
+            }
+
+            return Text("  لا يوجد اي عقود ");
+          });
 
     //this is the end of commentchild
   }
@@ -143,22 +134,14 @@ class _commentState extends State<comment> {
           userImage: CommentBox.commentImageParser(
               imageURLorPath:
                   "https://cdn.onlinewebfonts.com/svg/img_322817.png"),
-          child: commentChild(filedata), //هون بدنا نحط ال Query
+          child: commentChild(), //هون بدنا نحط ال Query
           labelText: 'اكتب تعليقك هنا...',
           errorText: 'Comment cannot be blank',
           withBorder: false,
           sendButtonMethod: () {
             if (formKey.currentState!.validate()) {
               print(commentController.text);
-              setState(() {
-                var value = {
-                  'name': '${snapshot.data!.docs[i]["user_name"]}',
-                  'pic': 'https://cdn.onlinewebfonts.com/svg/img_322817.png',
-                  'message': commentController.text,
-                  'date': '2021-01-01 12:00:00'
-                };
-                filedata.insert(0, value);
-              });
+             Add();
               commentController.clear();
               FocusScope.of(context).unfocus();
             } else {
@@ -174,4 +157,72 @@ class _commentState extends State<comment> {
       ),
     );
   }
+  fetchData() async{
+   
+  QuerySnapshot snapshot = await userref.get();
+  List<QueryDocumentSnapshot> listdocs = snapshot.docs;
+  return listdocs;
 }
+ Add() async {
+      userref.add({
+        "text": commentController.text,
+      }).then((value) {
+         Get.snackbar(
+        "تمت العملية بنجاح  ",
+        " ",
+        colorText: Colors.white,
+        backgroundColor: Colors.lightBlue,
+        icon: const Icon(Icons.add_alert),
+      snackPosition:SnackPosition.BOTTOM
+      );
+      }).catchError((e) {
+        print(e);
+      });
+    }
+  
+}
+
+
+
+
+
+
+        //   ListView(
+        //   children: [
+        //     for (var i = 0; i < snapshot.data!.docs.length; i++)
+        //       Padding(
+        //         padding: const EdgeInsets.fromLTRB(2.0, 8.0, 2.0, 0.0),
+        //         child: ListTile(
+        //           leading: GestureDetector(
+        //             onTap: () async {
+        //               // Display the image in large form.
+        //               print("Comment Clicked");
+        //             },
+        //             child: Container(
+        //               height: 50.0,
+        //               width: 50.0,
+        //               decoration: new BoxDecoration(
+        //                   color: Colors.black87,
+        //                   borderRadius:
+        //                       new BorderRadius.all(Radius.circular(50))),
+        //               child: CircleAvatar(
+        //                   radius: 50,
+        //                   backgroundImage: CommentBox.commentImageParser(
+        //                       imageURLorPath:
+        //                           'https://cdn.onlinewebfonts.com/svg/img_322817.png')),
+        //             ),
+        //           ),
+        //           title: Text(
+        //             "${snapshot.data!.docs[i]['user_name']}",
+        //             style: TextStyle(fontWeight: FontWeight.bold),
+        //           ),
+        //           subtitle: Text(
+        //             "${snapshot.data!.docs[i]["text"]}",
+        //           ),
+        //           trailing: Text("${snapshot.data!.docs[i]["date"]}",
+        //               style: TextStyle(fontSize: 10)),
+        //         ),
+        //       )
+        //   ],
+        // );
+        //${snapshot.data[i]['text']}
